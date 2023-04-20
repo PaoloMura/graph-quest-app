f5e053c76c671902d3987e95e1bd168a96f4b025
PaoloMura <paolo@muraonline.com>
2023-04-20 16:27:04 +0100

    improved the general node label positioning algorithm

--- src/components/utilities/label-positioning.js
+++ src/components/utilities/label-positioning.js
@@ -1,4 +1,5 @@
 // Zone proportions (T=top, B=bottom, L=left, R=right), clockwise round the circle.
+
 const TR = 1 / 16
 const RT = 3 / 16
 const RB = 5 / 16
@@ -8,9 +9,9 @@ const LB = 11 / 16
 const LT = 13 / 16
 const TL = 15 / 16
 
-export function setLabelPosCircle (node, n_nodes) {
+export function setLabelPosCircle (node, numNodes) {
   const v = node.data('value')
-  const pos = v / n_nodes
+  const pos = v / numNodes
   if (BL <= pos && pos < TL) {
     node.style('text-halign', 'left')
     node.style('text-margin-x', -10)
@@ -48,61 +49,70 @@ export function setLabelPosBipartite (node) {
 }
 
 export function setLabelPos (node) {
-  // Define 8 zones for the circle region around the node.
-  let zones = [
-    { x: 'center', y: 'top', mx: 0, my: -5, 'occupied': false },
-    { x: 'right', y: 'top', mx: 5, my: -5, 'occupied': false },
-    { x: 'right', y: 'center', mx: 5, my: 0, 'occupied': false },
-    { x: 'right', y: 'bottom', mx: 5, my: 5, 'occupied': false },
-    { x: 'center', y: 'bottom', mx: 0, my: 5, 'occupied': false },
-    { x: 'left', y: 'bottom', mx: -5, my: 5, 'occupied': false },
-    { x: 'left', y: 'center', mx: -5, my: 0, 'occupied': false },
-    { x: 'left', y: 'top', mx: -5, my: -5, 'occupied': false },
-  ]
-  // Determine which zones are occupied by the neighbouring edges.
-  const u = node.position('x'), v = node.position('y')
+  // 1. Find the angle that each neighbouring edge makes to the vertical
+  const angles = []
   const neighbours = node.neighbourhood().filter('node')
-  for (let n of neighbours) {
-    const a = n.position('x'), b = n.position('y')
+  const u = node.position('x'); const v = node.position('y')
+  for (const n of neighbours) {
+    const a = n.position('x'); const b = n.position('y')
     const x = u - a
     // Avoid division by zero
     const y = b - v === 0 ? 0.0001 : b - v
     // Add an offset of π to the angle to map it to the range 0..2π
     const angle = Math.atan2(x, y) + Math.PI
-    // + π/8 shifts the zone lines round the circle by half a segment;
-    // floor(_ / (π/4)) maps to an integer zone (8 zones take up π/4 radians each);
-    // % 8 keeps it in range 0..7
-    const zone_id = Math.floor((angle + (Math.PI / 8)) / (Math.PI / 4)) % 8
-    zones[zone_id].occupied = true
+    angles.push(angle)
   }
-  // Count the adjacent unoccupied zones forwards and backwards.
-  let xs = []
-  let xs_max = 0
-  for (let i = 0; i < 8; i++) {
-    if (!zones[i].occupied) {
-      i === 0 ? xs.push(1) : xs.push(xs[i - 1] + 1)
-      if (xs[i] > xs[xs_max]) xs_max = i
-    } else xs.push(0)
-  }
-  let ys = []
-  for (let i = 7; i >= 0; i--) {
-    if (!zones[i].occupied) {
-      i === 7 ? ys.push(1) : ys.push(ys[7 - i - 1] + 1)
-    } else ys.push(0)
-  }
-  // Find the midpoint of the largest contiguous region of unoccupied zones.
-  let s, e, mid
-  const wrap_max = xs[7] + ys[7]
-  if (wrap_max > xs[xs_max]) {
-    s = 7 - xs[7] + 1
-    e = 7 + ys[7]
+
+  // 2. We want to determine the optimum angle for the node label...
+  let myAngle
+  const gaps = []
+
+  // 3. If there are no neighbours, default to above
+  if (angles.length === 0) myAngle = 0
+  // 4. If there is only one neighbour, the ideal position is opposite
+  else if (angles.length < 2) {
+    myAngle = (angles[0] + Math.PI) % (2 * Math.PI)
   } else {
-    s = xs_max - xs[xs_max] + 1
-    e = xs_max
+    // 5. Otherwise, find the size of the gaps between each angle
+    angles.sort()
+    for (let i = 0; i < angles.length; i++) {
+      const j = (i + 1) % angles.length
+      let gap
+      if (i === angles.length - 1) gap = (2 * Math.PI - angles[i]) + angles[j]
+      else gap = angles[j] - angles[i]
+      gaps.push(gap)
+    }
+
+    // 6. Then, the ideal position is the midpoint of the largest gap
+    const largest = Math.max(...gaps)
+    const start = angles[gaps.indexOf(largest)]
+    myAngle = (start + 0.5 * largest) % (2 * Math.PI)
   }
-  mid = Math.floor(0.5 * (s + e)) % 8
-  // Set the label to be in this zone.
-  let zone = zones[mid]
+
+  // 7. Find the corresponding zone of this angle
+  const zones = [
+    { x: 'center', y: 'top', mx: 0, my: -5 },
+    { x: 'right', y: 'top', mx: 5, my: -5 },
+    { x: 'right', y: 'center', mx: 5, my: 0 },
+    { x: 'right', y: 'bottom', mx: 5, my: 5 },
+    { x: 'center', y: 'bottom', mx: 0, my: 5 },
+    { x: 'left', y: 'bottom', mx: -5, my: 5 },
+    { x: 'left', y: 'center', mx: -5, my: 0 },
+    { x: 'left', y: 'top', mx: -5, my: -5 }
+  ]
+  // + π/8 shifts the zone lines round the circle by half a segment;
+  // floor(_ / (π/4)) maps to an integer zone (8 zones take up π/4 radians each);
+  // % 8 keeps it in range 0..7
+  const zoneID = Math.floor((myAngle + (Math.PI / 8)) / (Math.PI / 4)) % 8
+
+  console.log('node:', node.data('id'))
+  console.log('angles:', angles)
+  console.log('gaps:', gaps)
+  console.log('myAngle:', myAngle)
+  console.log('zoneID:', zoneID)
+
+  // 8. Finally, set the label to be in this zone.
+  const zone = zones[zoneID]
   node.style('text-halign', zone.x)
   node.style('text-valign', zone.y)
   node.style('text-margin-x', zone.mx)
